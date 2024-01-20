f05c23fe8a05372e117c0fcc829878dce1dc29c8
@Dev_murli <42312132+murli1998@users.noreply.github.com>
2024-01-20 15:19:27 +0530

    js variables

---
+++ js_variable.js
@@ -0,0 +1,15 @@
+// variable are containers for any data to stored
+
+firstName = "murli";
+lastName = "manohar";
+age = 21;
+height = 1.63;
+a = null;
+y = undefined;
+isFollow = true;
+console.log(firstName+lastName);
+console.log(age);
+console.log(height);
+console.log(a);
+console.log(y);
+console.log(isFollow);
