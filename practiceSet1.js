3d5a94e1df64998343a69b5fef57195e7113123b
@Dev_murli <42312132+murli1998@users.noreply.github.com>
2024-01-20 17:32:16 +0530

    operators in javascript

--- practiceSet1.js
+++ practiceSet1.js
@@ -4,6 +4,64 @@ const product ={
     rating : 4,
     offer : 5,
     price : 270
-}
+};
 
 console.log(product);
+
+// creating another object ..... profile 
+
+const profile ={
+    username : "Shradha khapra",
+    isFollow : false,
+    followers : 123,
+    following : 1234
+};
+
+console.log(typeof profile["isFollow"]);
+console.log(typeof profile["username"]);
+console.log(profile.username);
+
+
+// // Operators in Javascript-- operators used to perform some operation on data.
+
+// -- Arithmetic 
+// -- Modulas 
+// -- Exponentiaition
+// -- Increement
+// -- Decrement
+
+let a = 10;
+let b = 5;
+console.log("a = ",a,"& b = ",b)
+console.log("a + b =",a+b);
+console.log("a - b =",a-b);
+console.log("a + b =",a*b);
+console.log("a + b =",a/b);
+console.log("a % b =",a%b);
+console.log("a ** b =",a**b);
+console.log("a + 1 =",++a);
+console.log("a - 1 =",--a);
+
+
+// assignment operators in javascript
+// = , += ,-= ,*=, /=, %=, **=
+
+console.log("a + 4 = ",a+=4);
+console.log("a - 4 = ",a-=4);
+console.log("a * 4 = ",a*=4);
+console.log("a / 4 = ",a/=4);
+console.log("a % 4 = ",a%=4);
+console.log("a ** 4 = ",a**=4);
+
+
+// comparison operators
+// == , != , === , !== , < , > , <= , >=
+
+let x = 5;
+let y = 2;
+
+let w = 4;
+let z = "4";
+console.log(x==y);
+console.log(w==z);
+console.log(w===z);
