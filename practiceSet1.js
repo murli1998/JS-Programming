f1c5e35696b54d19cd46effd11acc95ef3b639b5
@Dev_murli <42312132+murli1998@users.noreply.github.com>
2024-01-24 12:13:51 +0530

    user input

--- practiceSet1.js
+++ practiceSet1.js
@@ -78,3 +78,69 @@ let cond2 = w>z;
 console.log(cond1 && cond2);
 console.log(cond1 || cond2);
 
+// conditional statements 
+
+let age = 17;
+if(age>=18){
+    console.log("You can vote !");
+}else
+{
+    console.log("You can not vote !");
+}
+
+
+// if(cond1...){
+//     -------------
+//     -------------
+// }else if(cond2){
+//     -------------
+//     -------------
+// }else if(cond3){
+//     -------------
+//     --------------
+// }else if(cond4){
+//     --------------
+//     --------------
+// }else{
+//     -------------
+//     -------------
+// }
+
+let age2 = 58;
+if(age2<=18){
+    console.log("junior");
+}else if(age2>=60){
+    console.log("senior");
+}else{
+    console.log("middle");
+}
+
+// switch statements
+
+let fruit = "mango";
+switch(fruit){
+    case  "apple":
+        console.log("apple");
+        break;
+    case "banana":
+        console.log("his is banana");
+        break;
+    case "papaya":
+        console.log("This is papaya");
+        break;
+    case "mango":
+        console.log("This is mango");
+        break;
+    default:                                   //if none of the case matches with the expression then this default case will execute.
+        console.log("No fruit found");
+        break;   
+}
+
+
+
+// how we can get user input in javascript --------------- this will be done with the help of keyword -- prompt("enter your name");
+// prompt("what do you want to enter");
+
+alert("hello");
+let myName=prompt("Enter your name");
+console.log(myName);
