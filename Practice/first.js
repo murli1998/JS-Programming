47aca3b1124b41580f75b7a1e32adb69260239b8
@Dev_murli <42312132+murli1998@users.noreply.github.com>
2024-02-01 16:23:15 +0530

    ..

--- Practice/first.js
+++
@@ -1,5 +0,0 @@
-// console.log("Text to be displayed on console"); this is used to display text on console.
-console.log("Hello JavaScript");
-console.log("This is murli manohar !");
-console.log("I love javascript.");
-
